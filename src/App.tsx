a50a41798a7d192bf62b66166549ce0131238fc9
Martin Bartos <brtsmrtn@gmail.com>
2021-01-07 14:31:40 +0100

    build fix #10

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React from "react";
-//import logo from './logo.svg';
-import "./v1.css";
+import React, { useState } from "react";
+import { AddLink } from "./components/AddLink";
+import { SubmitLink } from "./components/SubmitLink";
+import { Message } from "./components/Message";
 
-function App() {
+const App = () => {
+  const submitForm = (event: React.FormEvent<HTMLInputElement>) => {
+    //   //this.setState({ disabled: e. });
+  };
+
+  const [url, setUrl] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p> Edit <code>src/App.tsx</code> and save to reload. </p> */}
-        {/* <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > Learn React </a> */}
-      </header>
+    <div>
+      <AddLink url={url} onChange={setUrl} />
+      <SubmitLink url={url} onClick={submitForm} />
+      <Message url={url}></Message>
+      {/* <Listing items={items}></Listing> */}
     </div>
   );
-}
+};
 
 export default App;
