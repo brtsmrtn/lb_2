d6fa0d86c5e0ed973bdf381f2897d3eff5f9b3ae
vanekerik <erik.vanek@infarm.com>
2021-01-08 13:44:08 +0100

    introduce github actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
-import { AddLink } from "./components/AddLink";
-import { SubmitLink } from "./components/SubmitLink";
-import { Message } from "./components/Message";
+import React, { useState } from 'react';
+import { AddLink } from './components/AddLink';
+import { SubmitLink } from './components/SubmitLink';
+import { Message } from './components/Message';
 
 const App = () => {
-  const submitForm = (event: React.FormEvent<HTMLInputElement>) => {
-    //   //this.setState({ disabled: e. });
-  };
+    const submitForm = (event: React.FormEvent<HTMLInputElement>) => {
+        //   //this.setState({ disabled: e. });
+    };
 
-  const [url, setUrl] = useState("");
-  return (
-    <div>
-      <AddLink url={url} onChange={setUrl} />
-      <SubmitLink url={url} onClick={submitForm} />
-      <Message url={url}></Message>
-      {/* <Listing items={items}></Listing> */}
-    </div>
-  );
+    const [url, setUrl] = useState('');
+    return (
+        <div style={{ backgroundColor: 'pink' }}>
+            <AddLink url={url} onChange={setUrl} />
+            <SubmitLink url={url} onClick={submitForm} />
+            <Message url={url}></Message>
+            {/* <Listing items={items}></Listing> */}
+        </div>
+    );
 };
 
 export default App;
