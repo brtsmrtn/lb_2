498181fdccd2d7abaec00ac7978e2af3134ac852
Martin Bartos <brtsmrtn@gmail.com>
2021-01-15 16:47:27 +0100

    fix build bambilion

--- src/components/ListingItem.tsx
+++ src/components/ListingItem.tsx
@@ -9,6 +9,7 @@ export class ListingItem extends React.Component<ListingItemProps> {
   render() {
     return (
       <tr>
+        <td>{this.props.item.id}</td>
         <td>{this.props.item.url}</td>
         <td>{this.props.item.date}</td>
       </tr>
