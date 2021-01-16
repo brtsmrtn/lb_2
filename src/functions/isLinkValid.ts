89f8699865a0884910c572408f9ba5c365dc4b5a
Martin Bartos <brtsmrtn@gmail.com>
2021-01-16 18:16:05 +0100

    isUrl validate fix

--- src/functions/isLinkValid.ts
+++ src/functions/isLinkValid.ts
@@ -1,4 +1,5 @@
-import isURL from "validator/lib/isURL";
+import validator from "validator";
+import isURL from "validator/es/lib/isURL";
 
 export function isLinkValid(url: string): boolean {
   return isURL(url);
