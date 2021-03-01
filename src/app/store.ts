8c992759a0b1c9aae72f13d8a211dc53ca4b845a
Martin Bartos <brtsmrtn@gmail.com>
2021-03-01 15:57:29 +0100

    local storage added

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import {
-  knownTagsReducer,
-  KnownTagsActions,
-  KnownTagsState,
-} from "../features/tags";
-import { itemsReducer, ItemsActions, ItemsState } from "../features/items";
-import {
-  urlInputReducer,
-  UrlInputActions,
-  UrlInputState,
-} from "../features/urlInput";
-import { TabsActions, tabsReducer, TabsState } from "../features/tabs";
+import { combineReducers, createStore } from "@reduxjs/toolkit";
+import { knownTagsReducer, KnownTagsState } from "../features/tags";
+import { itemsReducer, ItemsState } from "../features/items";
+import { urlInputReducer, UrlInputState } from "../features/urlInput";
+import { tabsReducer, TabsState } from "../features/tabs";
 
 export type ApplicationState = {
   knownTags: KnownTagsState;
@@ -18,14 +10,33 @@ export type ApplicationState = {
   urlInput: UrlInputState;
   tabs: TabsState;
 };
-export default configureStore<
-  ApplicationState,
-  KnownTagsActions & ItemsActions & UrlInputActions & TabsActions
->({
-  reducer: {
-    knownTags: knownTagsReducer,
-    items: itemsReducer,
-    urlInput: urlInputReducer,
-    tabs: tabsReducer,
-  },
+
+function saveToLocalStorage(state: ApplicationState) {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("LinkBiscuit", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem("LinkBiscuit");
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+}
+const rootReducers = combineReducers({
+  knownTags: knownTagsReducer,
+  items: itemsReducer,
+  urlInput: urlInputReducer,
+  tabs: tabsReducer,
 });
+const store = createStore(rootReducers, loadFromLocalStorage());
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+export default store;
